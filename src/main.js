92fb3d1f32ae528d3cab32dec6ae72d107e0661e
pfrito <carlos.rolando.morell@gmail.com>
2024-04-07 12:14:47 -0400

    add dist

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import './index.css'
-import '@mdi/font/css/materialdesignicons.css'
-
-createApp(App).mount('#app')
